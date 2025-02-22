9e3702141f298aceb2d0f844a93d1e31aad744f2
NastyaRebrova <rebrrova.nastya@gmail.com>
2025-02-22 10:27:59 +0300

    get

---
+++ get/index.php
@@ -0,0 +1,34 @@
+<?php
+// 26 задание
+
+    // if (isset($_GET['number'])) {
+    //     $number = $_GET['number'];
+    //     echo "Число: " . $number;
+    // } else {
+    //     echo "Число не было передано.";
+    // }
+
+// 27 задание
+    // if (isset($_GET['number'])) {
+    //     $number = $_GET['number'];
+    //     if ($number==1){
+    //         echo "Привет";
+    //     } elseif ($number == 2) {
+    //         echo "пока";
+    //     } else {
+    //         echo "Введено не 1 и не 2";
+    //     }
+    // } else {
+    //     echo "Число не было передано";
+    // }
+
+// 28 задание
+    if (isset($_GET['number1']) && isset($_GET['number2'])) {
+        $number1 = $_GET['number1'];
+        $number2 = $_GET['number2'];
+        $sum = $number1 + $number2;
+        echo "Сумма: " . $sum;
+    } else {
+        echo "Числа не были переданы.";
+    }
+?>
