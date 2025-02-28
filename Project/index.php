5b5f9e7b730213ba0fcf8071d6709bc85f790608
NastyaRebrova <rebrrova.nastya@gmail.com>
2025-03-01 01:17:43 +0300

    calculator_front

--- Project/index.php
+++ Project/index.php
@@ -149,25 +149,25 @@
     // echo '<BR>';
     // echo $b->sayHello();
 
-    class A {
-        public function method1(){
-            return $this->method2();
-        }
-        protected function method2(){
-            return 'A';
-        }
-    }
+    // class A {
+    //     public function method1(){
+    //         return $this->method2();
+    //     }
+    //     protected function method2(){
+    //         return 'A';
+    //     }
+    // }
 
-    class B extends A
-    {
-        protected function method2(){
-            return 'B';
-        }
-    }
-    $a = new A;
-    $b = new B;
-    echo $a->method1();
-    echo $b->method1();
+    // class B extends A
+    // {
+    //     protected function method2(){
+    //         return 'B';
+    //     }
+    // }
+    // $a = new A;
+    // $b = new B;
+    // echo $a->method1();
+    // echo $b->method1();
 
 
 
